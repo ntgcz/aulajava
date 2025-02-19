3bbbb928661b1f0127b81a532290f83d61d1bbe2
ntgcz <natgomescruz@gmail.com>
2025-02-18 21:35:34 -0300

    Add files via upload

---
+++ MeuPrograma.java
@@ -0,0 +1,19 @@
+// Meu primeiro código java
+
+// Esta linha é um comentário
+
+/*
+atalhos:
+class - criar o bloco da classe
+main - criar o bloco do main 
+sout - criar o print
+ 
+ */
+
+
+public class MeuPrograma {// nome do programa 
+
+    public static void main(String[] args) {
+        System.out.println("Boa noite!");
+    }
+}
