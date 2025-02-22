c46ee614bc39a4510b4c4f49fc91f5f0f94d0fed
ntgcz <natgomescruz@gmail.com>
2025-02-22 02:13:18 -0300

    Add files via upload

---
+++ Teste.java
@@ -0,0 +1,15 @@
+public class Teste {
+
+    public static void main(String[] args) {
+        System.out.print("Oii\nTchau\nOu Não\n");
+        System.out.println(10+20);
+        System.out.println(10*20);
+        System.out.println(10/20);
+        System.out.println(10-20);
+        System.out.println(10%20);
+       System.out.println("resultado = " + (10+10));
+
+
+
+    }
+}
